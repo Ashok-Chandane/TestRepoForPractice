b79045676a59aac9db95a9bb2367269357dfaff6
Ashok-Chandane <ashok.chandane5@gmail.com>
2025-10-08 21:17:40 +0200

    Changes updated

--- PythonCode/TestFolder/TestFile.py
+++ PythonCode/TestFolder/TestFile.py
@@ -1,3 +1,8 @@
 
-for i in range(2,22,2):
-    print(i)
+multiplier = 5
+print(f"This is the table of {multiplier}")
+
+for i in range(1,11):
+    print(f"{multiplier} x {i} = {i * multiplier}")
+else : 
+    print("End of Table")
